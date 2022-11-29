276fbc896e7a6ff1162fe0c86c507cc3205d437b
vladimir <vszkylnyj@gmail.com>
2022-11-29 20:54:00 -0300

    Agrego un gatito

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="imagenes/gatito.jpg" alt="Gatito" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
